96f636c844d2bc75ec252f939bd4f640b887a51b
Noran Fawzy <noranfawzy05@gmail.com>
2026-07-16 12:38:50 +0300

    Add files via upload

---
+++ Day-4/external-3a.js
@@ -0,0 +1,8 @@
+// 3-a
+console.log(number1);
+ //console.log(number2);
+ //console.log(number3);
+
+var number1 = 3;
+let number2 = 2.9;
+const number3 = 0xff;
---
+++ Day-4/external.js
@@ -0,0 +1,30 @@
+// 2-a
+var number1 = 3;
+let number2 = 2.9;
+const number3 = 0xff;
+
+// 2-b
+var firstName = "Noran";
+var middleName = 'Fawzy';
+var lastName = `Najm`;
+
+// 2-c
+var flag = true;
+
+// 2-d
+console.log("This is the External JavaScript file");
+
+// 3-g
+firstName[3] = 'A';
+
+console.log(firstName);
+
+console.log(typeof number1);
+console.log(typeof number2);
+console.log(typeof number3);
+
+console.log(typeof firstName);
+console.log(typeof middleName);
+console.log(typeof lastName);
+
+console.log(typeof flag);
